38947fc5b24caab2dea6e4651efd61efc944f954
zloyleva <zloyleva@gmail.com>
2017-02-05 21:47:43 +0200

    Get start offset from string

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# price_parser
+# PHP price parser
+<h2>PHP price parser for WooCommerce E-store.</h2> 
+
+<p>Ver. 1.0</p>
--- parser_row.php
+++ parser_row.php
@@ -1,7 +1,9 @@
 <?php
   function parser_row($row){
     static $price_content = 0;
-    if (preg_match('@1. Наша продукция@', $row, $match)):
+    if (preg_match('@(^\s+)(1. Наша продукция)@', $row, $match)):
+//        Get start offset from string
+        $offset_srt = strlen($match[1]);
         $price_content = 1;
     endif;
     if (1 == $price_content):
